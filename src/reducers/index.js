1b3183e5965305202d1f60ece85ffe8a5dc40bb7
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-11-19 22:41:25 +0000

    added qt access reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,36 @@
 import { combineReducers } from 'redux'
 import * as types from '../action-types'
 
-const qtAccessInitalState = {
+const qtaccess = (state = {
   isFetching: false,
-  items: []
-}
-const qtaccess = (state = qtAccessInitalState, action) => {
+  items: [],
+  entities: {}
+}, action) => {
   switch(action.type) {
-    case types.FETCH_QT_ACCESS:
+    case types.FETCH_QT_ACCESS_REQUEST:
       return Object.assign({}, state, {
         isFetching: true
+      })
+    case types.FETCH_QT_ACCESS_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false
+      })
+    case types.FETCH_QT_ACCESS_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.data.map(qtaccess => qtaccess.id),
+        entities: action.data.reduce( (entities, qtaccess) => {
+          entities[qtaccess.id] = qtaccess
+          return entities
+        }, {})
       })
     default:
       return state
   }
 }
 
-const NBReducer = combineReducers({
+const nbReducer = combineReducers({
   qtaccess
 })
 
-export default NBReducer
+export default nbReducer
--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -1,29 +1,73 @@
 import * as actionTypes from '../src/action-types'
 import * as actions from '../src/actions'
-import NBReducer from '../src/reducers'
-
-import * as endpoints from './nabla-api-mocks'
+import nbReducer from '../src/reducers'
 
 const initialState = {
   qtaccess: {
     isFetching: false,
-    items: []
+    items: [],
+    entities: {}
   }
 }
 
 describe('reducers', () => {
 
   it('should return inital state', () => {
-    expect(NBReducer(undefined, {})).toEqual(initialState)
+    expect(nbReducer(undefined, {})).toEqual(initialState)
   })
 
-  it('should start fetching qt access', () => {
-    const expectedState = {
+  it('should handle FETCH_QT_ACCESS_REQUEST', () => {
+    expect(nbReducer(initialState, actions.fetchQTAccessRequest())).toEqual({
       qtaccess: {
         isFetching: true,
-        items: []
+        items: [],
+        entities: {}
+      }
+    })
+  })
+
+  it('should handle FETCH_QT_ACCESS_FAILURE', () => {
+    expect(nbReducer(initialState, actions.fetchQTAccessFailure())).toEqual({
+      qtaccess: {
+        isFetching: false,
+        items: [],
+        entities: {}
+      }
+    })
+  })
+
+  it('should handle FETCH_QT_ACCESS_SUCCESS', () => {
+    const state = {
+      qtaccess: {
+        isFetching: true,
+        items: [],
+        entities: {}
       }
     }
-    expect(NBReducer(initialState, actions.fetchQTAccess())).toEqual(expectedState)
+    const data = [{
+      'id': '579d6115-b0f6-4c6c-b9ff-cfc28692d532',
+      'modified': '2000-01-29T12:00:00.000000Z',
+      'scope': 'ACC',
+      'access_token': 'C3lTUKuNQrAAmSD/TPjuV/HI7aNrAwDp',
+      'refresh_token': 'aSBe7wAAdx88QTbwut0tiu3SYic3ox8F',
+      'api_server': 'https://api01.iq.questrade.com/'
+    }]
+    const successAction = actions.fetchQTAccessSuccess(data)
+    expect(nbReducer(undefined, successAction)).toEqual({
+      qtaccess: {
+        isFetching: false,
+        items: ['579d6115-b0f6-4c6c-b9ff-cfc28692d532'],
+        entities: {
+          [data[0].id]: {
+            'id': '579d6115-b0f6-4c6c-b9ff-cfc28692d532',
+            'modified': '2000-01-29T12:00:00.000000Z',
+            'scope': 'ACC',
+            'access_token': 'C3lTUKuNQrAAmSD/TPjuV/HI7aNrAwDp',
+            'refresh_token': 'aSBe7wAAdx88QTbwut0tiu3SYic3ox8F',
+            'api_server': 'https://api01.iq.questrade.com/'
+          }
+        }
+      }
+    })
   })
 })
