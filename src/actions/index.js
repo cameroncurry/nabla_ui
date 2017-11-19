482f6c63106a8d7ea2b351beb4cb341b052ee25e
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-11-19 19:23:17 +0000

    changed axios middleware for thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,27 @@
-
+import axios from 'axios'
 import * as types from '../action-types'
 
-export const fetchQTAccess = () => ({
-  type: types.FETCH_QT_ACCESS,
-  payload: {
-    request: {
-      url: '/api/qtaccess'
-    }
-  }
+export const fetchQTAccessRequest = () => ({
+  type: types.FETCH_QT_ACCESS_REQUEST
 })
+
+export const fetchQTAccessSuccess = data => ({
+  type: types.FETCH_QT_ACCESS_SUCCESS,
+  data
+})
+
+export const fetchQTAccessFailure = error => ({
+  type: types.FETCH_QT_ACCESS_FAILURE,
+  error
+})
+
+export function fetchQTAccess() {
+  return dispatch => {
+    dispatch(fetchQTAccessRequest())
+    return axios.get('/api/qtaccess').then(response => {
+      dispatch(fetchQTAccessSuccess(response.data))
+    }).catch(error => {
+      dispatch(fetchQTAccessFailure(error))
+    })
+  }
+}
--- tests/actions.test.js
+++ tests/actions.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import axiosMiddleware from 'redux-axios-middleware'
+import thunk from 'redux-thunk'
 import AxiosMockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
 
@@ -8,50 +8,28 @@ import * as actionTypes from '../src/action-types'
 
 import * as endpoints from './nabla-api-mocks'
 
-let axiosClient = axios.create()
-let middleware = [axiosMiddleware(axiosClient)]
-let mockStore = configureMockStore(middleware)
+const middleware = [thunk]
+const mockStore = configureMockStore(middleware)
 
 
 describe('actions', () => {
 
-  it('should create fetch qt access action', () => {
-    const expectedAction = {
-      type: actionTypes.FETCH_QT_ACCESS,
-      payload: {
-        request: {
-          url: '/api/qtaccess'
-        }
-      }
-    }
-    expect(actions.fetchQTAccess()).toEqual(expectedAction)
-  })
-
   it('should fetch qt access', () => {
-    let mock = new AxiosMockAdapter(axiosClient)
-    mock.onGet(endpoints.API_QT_ACCESS.endpoint).reply(200, endpoints.API_QT_ACCESS.data)
+    let axiosMock = new AxiosMockAdapter(axios)
+    axiosMock.onGet(endpoints.API_QT_ACCESS.endpoint).reply(200, endpoints.API_QT_ACCESS.data)
 
-    const expectedAction = {
-      type: actionTypes.FETCH_QT_ACCESS,
-      payload: {
-        request: {
-          url: '/api/qtaccess'
-        }
+    const expectedActions = [
+      {
+        type: actionTypes.FETCH_QT_ACCESS_REQUEST
+      },
+      {
+        type: actionTypes.FETCH_QT_ACCESS_SUCCESS,
+        data: endpoints.API_QT_ACCESS.data
       }
-    }
-
-    const expectedType = actionTypes.FETCH_QT_ACCESS_SUCCESS
-    const expectedData = endpoints.API_QT_ACCESS.data
-
+    ]
     const store = mockStore()
-    return store.dispatch(actions.fetchQTAccess())
-      .then(() => {
-        const dispatchedActions = store.getActions()
-        expect(dispatchedActions.length).toEqual(2)
-        expect(dispatchedActions[0]).toEqual(expectedAction)
-        expect(dispatchedActions[1].type).toEqual(expectedType)
-        expect(dispatchedActions[1].payload.status).toEqual(200)
-        expect(dispatchedActions[1].payload.data).toEqual(expectedData)
-      })
+    return store.dispatch(actions.fetchQTAccess()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions)
+    })
   })
 })
