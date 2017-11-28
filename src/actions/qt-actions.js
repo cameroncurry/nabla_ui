7312697158a7027695c345b41c6879e53b4e3588
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-11-28 19:48:57 +0000

    moved qt access actions to its own file

---
+++ src/actions/qt-actions.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import * as types from '../action-types'
+
+export const fetchQTAccessRequest = () => ({
+  type: types.FETCH_QT_ACCESS_REQUEST
+})
+
+export const fetchQTAccessSuccess = data => ({
+  type: types.FETCH_QT_ACCESS_SUCCESS,
+  data
+})
+
+export const fetchQTAccessFailure = error => ({
+  type: types.FETCH_QT_ACCESS_FAILURE,
+  error
+})
+
+export function fetchQTAccess() {
+  return dispatch => {
+    dispatch(fetchQTAccessRequest())
+    return axios.get('/api/qt-access').then(response => {
+      dispatch(fetchQTAccessSuccess(response.data))
+    }).catch(error => {
+      dispatch(fetchQTAccessFailure(error))
+    })
+  }
+}
+
+export const refershQTAccessRequest = () => ({
+  type: types.REFRESH_QT_ACCESS_REQUEST
+})
+
+export const refershQTAccessSuccess = data => ({
+  type: types.REFRESH_QT_ACCESS_SUCCESS,
+  data
+})
+
+export const refershQTAccessFailure = error => ({
+  type: types.REFRESH_QT_ACCESS_FAILURE,
+  error
+})
+
+export function refershQTAccess(id) {
+  return dispatch => {
+    dispatch(refershQTAccessRequest())
+    return axios.put(`/api/qt-access/${id}`).then(response => {
+      dispatch(refershQTAccessSuccess(response.data))
+    }).catch(error => {
+      dispatch(refershQTAccessFailure(error))
+    })
+  }
+}
