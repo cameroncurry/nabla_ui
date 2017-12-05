302191192ad6759747ac88bc3da63c9c6310d85f
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-12-05 22:27:19 +0000

    eslint fixes

--- src/components/NBHeader.jsx
+++ src/components/NBHeader.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
-
 import IconButton from 'material-ui/IconButton'
 import MoreVertIcon from 'material-ui-icons/MoreVert'
 import MenuIcon from 'material-ui-icons/Menu'
+import Chip from 'material-ui/Chip'
+import RefreshIcon from 'material-ui-icons/Refresh'
 
 const styles = theme => ({
   root: {
@@ -20,28 +22,46 @@ const styles = theme => ({
     marginLeft: -12,
     marginRight: 20,
   },
+  chip: {
+    margin: theme.spacing.unit
+  }
 })
 
 class NBHeader extends Component {
+
+  handleRefreshBalance = () => {
+    // console.log('refreshing balance')
+  }
   
   render() {
     const {classes} = this.props
     return (
       <AppBar position='static'>
         <Toolbar>
-          <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
+          <IconButton className={classes.menuButton} color='contrast' aria-label='Menu'>
             <MenuIcon />
           </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
+          <Typography type='title' color='inherit' className={classes.flex}>
             Nabla
           </Typography>
-          <IconButton color="contrast">
+          <Chip
+            label='8,769.61 (CAD)'
+            className={classes.chip}
+            onClick={undefined}
+            onRequestDelete={this.handleRefreshBalance}
+            deleteIcon={<RefreshIcon />}
+          />
+          <IconButton color='contrast'>
             <MoreVertIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
     )
   }
+}
+
+NBHeader.propTypes = {
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styles)(NBHeader)
