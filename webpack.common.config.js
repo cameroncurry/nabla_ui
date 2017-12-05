e83623ed2089b6d143551afb5651b9e876ed70d9
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-12-05 22:22:29 +0000

    added eslint to webpack build

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -5,6 +5,11 @@ module.exports = {
   },
   webpack_module: {
     rules: [{
+      enforce: 'pre',
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    },{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
